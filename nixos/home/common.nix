2e25feac1d07363a358780a6f0c20627dcd3111c
Vladimír Zahradník <lada.zahradnik@gmail.com>
2023-08-24 21:22:28 +0200

    add home state version

--- nixos/home/common.nix
+++ nixos/home/common.nix
@@ -4,6 +4,7 @@
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  home.stateVersion = "23.05";
   home.username = "wexder";
   home.homeDirectory = "/home/wexder";
 }
