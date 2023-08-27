f5811ca4ff8d205da9915095a06282d897f3ba11
Vladimír Zahradník <lada.zahradnik@gmail.com>
2023-08-27 16:17:48 +0200

    add packer

--- nixos/programs/nvim.nix
+++ nixos/programs/nvim.nix
@@ -6,4 +6,13 @@
     rev = "01c0d0024cce340170ec71e5f73bcfbd2e633e8d";
     sha256 = "0wzrb627508y5js2i6n75lprfhw8raxi82fvvzd4qkhw5ml8ligc";
   });
+
+  home.file."~/.local/share/nvim/site/pack/packer/start/packer.nvim".source = (
+    pkgs.fetchFromGitHub {
+      owner = "wbthomason";
+      repo = "packer.nvim";
+      rev = "ea0cc3c59f67c440c5ff0bbe4fb9420f4350b9a3";
+      sha256 = "0lvpflpm7mhv39dglxdvmilj72xgxx5j614ll06mcxycv2k3xcvw";
+    }
+  );
 }
