32b5c747529bcdc1352b15c4e1818cd2ace9b51b
Vladimír Zahradník <lada.zahradnik@gmail.com>
2023-08-27 16:08:57 +0200

    add sha

--- nixos/programs/nvim.nix
+++ nixos/programs/nvim.nix
@@ -3,5 +3,6 @@
   home.file.".config/nvim".source = (builtins.fetchGit {
     url = "ssh://git@github.com/wexder/nvim.IDE";
     ref = "master";
+    sha256 = "0wzrb627508y5js2i6n75lprfhw8raxi82fvvzd4qkhw5ml8ligc";
   });
 }
