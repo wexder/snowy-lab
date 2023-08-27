fdc53625ce68b336b94e52f0f41ac9865f2291ca
Vladimír Zahradník <lada.zahradnik@gmail.com>
2023-08-27 16:02:45 +0200

    fix nvim fetching

--- nixos/programs/nvim.nix
+++ nixos/programs/nvim.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 {
-  home.file.".config/nvim".source = (builtins.fetchGit {
-    url = "git@github.com/wexder/nvim.IDE";
+  home.file.".config/nvim".source = (pkgs.fetchFromGitHub {
+    url = "ssh://git@github.com/wexder/nvim.IDE";
     ref = "master";
   });
 }
