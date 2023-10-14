e2cabf0cbcdd386d8f727074527c6e4428b21ea8
Vladimír Zahradník <lada.zahradnik@gmail.com>
2023-10-14 21:44:24 +0200

    remove cloned nvim config

--- nixos/programs/nvim.nix
+++ nixos/programs/nvim.nix
@@ -4,10 +4,10 @@
     pkgs.vimPlugins.packer-nvim
   ];
 
-  home.file.".config/nvim".source = (pkgs.fetchFromGitHub {
-    owner = "wexder";
-    repo = "nvim.IDE";
-    rev = "01c0d0024cce340170ec71e5f73bcfbd2e633e8d";
-    sha256 = "0wzrb627508y5js2i6n75lprfhw8raxi82fvvzd4qkhw5ml8ligc";
-  });
+  # home.file.".config/nvim".source = (pkgs.fetchFromGitHub {
+  #   owner = "wexder";
+  #   repo = "nvim.IDE";
+  #   rev = "01c0d0024cce340170ec71e5f73bcfbd2e633e8d";
+  #   sha256 = "0wzrb627508y5js2i6n75lprfhw8raxi82fvvzd4qkhw5ml8ligc";
+  # });
 }
