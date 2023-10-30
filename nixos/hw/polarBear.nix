bfe3973c72c16fe4c9d76390a971febc2d4f0715
Vladimir Zahradnik <wexder19@gmail.com>
2023-10-30 12:14:12 +0100

    add backup disk to polar bear

--- nixos/hw/polarBear.nix
+++ nixos/hw/polarBear.nix
@@ -13,11 +13,16 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    {
+  fileSystems = {
+    "/" = {
       device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
+    "/backup" = {
+      device = "/dev/disk/by-label/backup";
+      fsType = "ext4";
+    };
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
