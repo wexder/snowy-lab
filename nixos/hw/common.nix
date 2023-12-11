1813f6d5cf2ff5fddea0450b5e69778feda160c4
Vladimir Zahradnik <wexder19@gmail.com>
2023-12-11 14:20:45 +0100

    update fs limits

--- nixos/hw/common.nix
+++ nixos/hw/common.nix
@@ -2,9 +2,9 @@
 {
   boot.kernel.sysctl = {
     # Note that inotify watches consume 1kB on 64-bit machines.
-    "fs.inotify.max_user_watches" = 32768; # default:  8192
-    "fs.inotify.max_user_instances" = 1024; # default:   128
-    "fs.inotify.max_queued_events" = 32768; # default: 16384
+    "fs.inotify.max_user_watches" = 65536; # default:  8192
+    "fs.inotify.max_user_instances" = 2048; # default:   128
+    "fs.inotify.max_queued_events" = 65536; # default: 16384
   };
 
   imports = [
