7abf01649efe561a2a6a4800f7fc3e0f560c3df9
Vladimir Zahradnik <wexder19@gmail.com>
2023-12-19 21:06:52 +0100

    add allow unfree in home manager

--- nixos/home/common.nix
+++ nixos/home/common.nix
@@ -7,6 +7,7 @@
   home.stateVersion = "23.11";
   home.username = "wexder";
   home.homeDirectory = "/home/wexder";
+  nixpkgs.config.allowUnfreePredicate = _: true;
 
   imports = [
     ../programs/nvim.nix
