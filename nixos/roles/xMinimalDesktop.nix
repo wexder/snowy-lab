3b887b3597f960debbd7373d1d4b219c74cc3386
Vladimir Zahradnik <wexder19@gmail.com>
2023-12-20 19:39:03 +0100

    add waypipe

--- nixos/roles/xMinimalDesktop.nix
+++ nixos/roles/xMinimalDesktop.nix
@@ -23,6 +23,7 @@ in
       services.xrdp.openFirewall = true;
 
       environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+        waypipe
         elisa
         gwenview
         okular
