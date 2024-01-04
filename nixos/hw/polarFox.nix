6bd16cdbcad63968c5d5074f48c0b69b2375dd0a
Vladimir Zahradnik <wexder19@gmail.com>
2024-01-04 21:19:56 +0100

    add polar fox rtl kernel module

--- nixos/hw/polarFox.nix
+++ nixos/hw/polarFox.nix
@@ -11,7 +11,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8812au ];
 
   fileSystems = {
     "/" = {
