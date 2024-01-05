9488cfdd64c8bc6bb1161774cb375c4c96ea85c4
Vladimir Zahradnik <wexder19@gmail.com>
2024-01-05 19:49:20 +0100

    cleanup

--- nixos/hw/polarFox.nix
+++ nixos/hw/polarFox.nix
@@ -9,10 +9,8 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  # boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
-  # boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl8812au
