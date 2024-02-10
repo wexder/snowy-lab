4d525f5b1b551647febd44aedcbe6be654a200a1
Vladimir Zahradnik <wexder19@gmail.com>
2024-02-10 14:20:30 +0100

    rekey for snowball

--- nixos/secrets/secrets.nix
+++ nixos/secrets/secrets.nix
@@ -5,8 +5,9 @@ let
   altostratus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDViCLCy9aIqaiTlKaHPlPPyJ+PPHZmKYbWMdTVg8pkv";
   polar-fox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBTlyCifL26ThYrCF+gx0VP+Fe5eKzc+E3F0NoOf9sD";
   polar-bear = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUiqS1ZGBpYEdV4D8TSE8/A94tnxLF9YYlUOU3yQHaa";
+  snowball = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6W1y/Xu1VfR6FKxl7yF1XcU3yNFlMDSdFvTrlXfcGu";
 
-  machines = [ altostratus polar-fox polar-bear ];
+  machines = [ altostratus polar-fox polar-bear snowball ];
 in
 {
   "tailscale.age".publicKeys = users ++ machines;
