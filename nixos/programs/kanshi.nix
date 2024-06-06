f7b0af75c8fa081ed96e9007b86466ab7f1404e5
Vladimir Zahradnik <wexder19@gmail.com>
2024-06-06 09:32:04 +0200

    tweak kanshi

--- nixos/programs/kanshi.nix
+++ nixos/programs/kanshi.nix
@@ -4,18 +4,54 @@
     enable = true;
     systemdTarget = "sway-session.target";
 
-    profiles = {
-      default = {
-        outputs = [
-          {
+    settings = [
+    {
+        profile.outputs = [
+        {
             criteria = "eDP-1";
             scale = 1.4;
-            status = "enable";
-          }
+        }
         ];
-      };
+    }
+    {
+        profile.outputs = [
+        {
+            criteria = "eDP-1";
+            scale = 1.4;
+            position = "5600,1231";
+        }
+        {
+            criteria = "DP-8";
+            mode = "3840x2160";
+            position = "0,0";
+        }
+        {
+            criteria = "DP-3";
+            mode = "3440x1440";
+            position = "2160,1231";
+        }
+        ];
+    }
+    {
+        profile.outputs = [
+        {
+            criteria = "eDP-1";
+            scale = 1.4;
+            position = "5600,1231";
+        }
+        {
+            criteria = "DP-9";
+            mode = "3840x2160";
+            position = "0,0";
+        }
+        {
+            criteria = "DP-6";
+            mode = "3440x1440";
+            position = "2160,1231";
+        }
+        ];
 
-      office = { };
-    };
+    }
+    ];
   };
 }
