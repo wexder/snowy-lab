7f7f090dd6c384d665c11f8a88824140f1daae1a
Vladimir Zahradnik <wexder19@gmail.com>
2025-07-20 09:43:13 +0200

    fix: games mkif

--- nixos/roles/linux/games.nix
+++ nixos/roles/linux/games.nix
@@ -19,32 +19,32 @@ in
 
   config =
     lib.mkIf cfg.enable
-      lib.mkMerge [
-      {
-        programs.steam = {
-          enable = true;
-        };
-        programs.steam.gamescopeSession = {
-          enable = true;
-        };
+      (lib.mkMerge [
+        {
+          programs.steam = {
+            enable = true;
+          };
+          programs.steam.gamescopeSession = {
+            enable = true;
+          };
 
-        programs.gamemode.enable = true;
+          programs.gamemode.enable = true;
 
-        environment.sessionVariables = {
-          STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-        };
+          environment.sessionVariables = {
+            STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+          };
 
-        environment.systemPackages = [
-          pkgs.protonup
-          pkgs.mangohud
-          pkgs.gamescope
-          pkgs.prismlauncher
-          pkgs.lutris
-          pkgs.moonlight-qt
-        ];
-      }
-      (lib.mkIf cfg.server {
-        services.sunshine.enable = true;
-      })
-    ];
+          environment.systemPackages = [
+            pkgs.protonup
+            pkgs.mangohud
+            pkgs.gamescope
+            pkgs.prismlauncher
+            pkgs.lutris
+            pkgs.moonlight-qt
+          ];
+        }
+        (lib.mkIf cfg.server {
+          services.sunshine.enable = true;
+        })
+      ]);
 }
