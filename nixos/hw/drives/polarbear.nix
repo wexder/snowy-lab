d4b5377b4cabe56f7ac31ea9423bd50feef14318
root <root@neurobug.com>
2025-09-25 22:23:50 +0200

    feat: refactor polar bear hw

---
+++ nixos/hw/drives/polarbear.nix
@@ -0,0 +1,69 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [
+                  "-L"
+                  "nixos"
+                  "-f"
+                ];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "subvol=root"
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "subvol=home"
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "subvol=nix"
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "/swap" = {
+                    mountpoint = "/swap";
+                    swap.swapfile.size = "64G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
--- nixos/hw/polarBear.nix
+++ nixos/hw/polarBear.nix
@@ -1,13 +1,11 @@
 { lib
-, config
-, pkgs
-, modulesPath
 , ...
 }:
 {
   boot.loader.systemd-boot.enable = true;
 
   imports = [
+    ./drives/polarbear.nix
     ./common.nix
     ./bluetooth.nix
     ./hid.nix
@@ -31,15 +29,6 @@
   ];
   hardware.ksm.enable = true;
 
-  services.openssh.enable = true;
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
-  };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -49,56 +38,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = 16382;
-    }
-  ];
-
   gpus.nvidia.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    nut
-  ];
-
-  environment.etc."passwordFile-ups".text = builtins.readFile ./polarBear/ups-pass;
-  # TODO Fix
-  environment.etc."passwordFile-upsm".text = builtins.readFile ./polarBear/upsdm-pass;
-
-  # power.ups = {
-  #   enable = true;
-  #   mode = "netserver";
-  #   openFirewall = true;
-  #   upsmon = {
-  #     monitor."serverups" = {
-  #       system = "serverups@localhost:3493";
-  #       user = "ups";
-  #       passwordFile = "/etc/passwordFile-ups";
-  #       powerValue = 2;
-  #     };
-  #   };
-  #   users = {
-  #     wexder = {
-  #       passwordFile = "/etc/passwordFile-upsm";
-  #       upsmon = "primary";
-  #       actions = [ "set" "fsd" ];
-  #       instcmds = [ "ALL" ];
-  #     };
-  #   };
-  #   upsd = {
-  #     listen = [
-  #       {
-  #         address = "0.0.0.0";
-  #         port = 3493;
-  #       }
-  #     ];
-  #   };
-  #   ups."serverups" = {
-  #     driver = "usbhid-ups";
-  #     port = "auto";
-  #     description = "Server UPS";
-  #     shutdownOrder = 0;
-  #   };
-  # };
 }
