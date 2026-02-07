bcb76ee8774834a034c3a5a5f256c51c085f8821
Vladimir Zahradnik <wexder19@gmail.com>
2026-02-07 20:32:01 +0100

    fix: fix intel gpu packages

--- nixos/hw/gpu/intel.nix
+++ nixos/hw/gpu/intel.nix
@@ -3,29 +3,29 @@
   pkgs,
   lib,
   ...
-}: let
+}:
+let
   cfg = config.gpus.intel;
-in {
+in
+{
   options.gpus.intel = {
     enable = lib.mkEnableOption "Enable amd intel";
   };
 
-  config =
-    lib.mkIf cfg.enable
-    {
-      services.xserver.enable = true;
-      services.xserver.videoDrivers = ["intel"];
-      # Enable OpenGL
-      hardware.graphics = {
-        enable = true;
-        enable32Bit = true;
-        extraPackages = [
-          pkgs.intel-media-driver # LIBVA_DRIVER_NAME=iHD
-          pkgs.vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-          pkgs.vaapiVdpau
-          pkgs.libvdpau-va-gl
-          pkgs.vpl-gpu-rt
-        ];
-      };
+  config = lib.mkIf cfg.enable {
+    services.xserver.enable = true;
+    services.xserver.videoDrivers = [ "intel" ];
+    # Enable OpenGL
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = [
+        pkgs.intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        pkgs.intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        pkgs.libva-vdpau-driver
+        pkgs.libvdpau-va-gl
+        pkgs.vpl-gpu-rt
+      ];
     };
+  };
 }
